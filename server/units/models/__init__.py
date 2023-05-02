a930e77a28185f39340f965833ecc8f0fca81ea3
D1ffic00lt <dm.filinov@gmail.com>
2023-05-02 17:55:31 +0700

    models version updated

--- server/units/models/__init__.py
+++ server/units/models/__init__.py
@@ -5,17 +5,17 @@ from .parts import *
 
 
 class _ModelsPaths(Enum):
-    xgb_fuel_130 = "./units/models/saves/fuel/xgb_fuelTA130_099889.bf"
-    catboost_fuel_130 = "./units/models/saves/fuel/cat_boost_fuelTA130_09924.bf"
+    xgb_fuel_130_v1 = "./units/models/saves/fuel/xgb_fuelTA130_099889.bf"
+    catboost_fuel_130_v1 = "./units/models/saves/fuel/cat_boost_fuelTA130_09924.bf"
 
 
 class Models(object):
     def __init__(self):
-        self.xgb_fuel_130 = XGBFuelModelV1(_ModelsPaths.xgb_fuel_130.value)
-        self.catboost_fuel_130 = CatBoostFuelModelV1(_ModelsPaths.catboost_fuel_130.value)
+        self.xgb_fuel_130_v1 = XGBFuelModelV1(_ModelsPaths.xgb_fuel_130_v1.value)
+        self.catboost_fuel_130_v1 = CatBoostFuelModelV1(_ModelsPaths.catboost_fuel_130_v1.value)
         self.__all_models = {
-            "xgb_fuel_130_v1": self.xgb_fuel_130,
-            "catboost_fuel_130_v1": self.catboost_fuel_130
+            "xgb_fuel_130_v1": self.xgb_fuel_130_v1,
+            "catboost_fuel_130_v1": self.catboost_fuel_130_v1
         }
 
     def __getitem__(self, item: str) -> List[int]:
