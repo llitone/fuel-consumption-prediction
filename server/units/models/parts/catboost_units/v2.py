e95b8b7755f838f81ea1e34960f1b29bc058f4d0
EgorAndrik <egorandreas28@gmail.com>
2023-05-03 23:29:48 +0700

    Add v2 class

---
+++ server/units/models/parts/catboost_units/v2.py
@@ -0,0 +1,16 @@
+import numpy as np
+from ..catboost import CatBoostFuelModel
+
+
+class CatBoostFuelModelV2(CatBoostFuelModel):
+    def __init__(self, model_path: str):
+        super().__init__(model_path)
+
+    def _convert_data(self, data: np.ndarray) -> np.ndarray:
+        data = self._convert(data)
+        if len(data.shape) == 1:
+            data = np.array([data])
+        return data
+
+    def predict(self, data: np.ndarray) -> np.ndarray:
+        return self.convert(self.model.predict(self._convert_data(data))).tolist()
