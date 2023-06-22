c124127a1f714c45a754941b5593ce5543a3e250
katikn <antoshapetlin@gmail.com>
2023-06-22 15:40:47 +0700

    site changes

--- client/preview/site/routes/main.js
+++ client/preview/site/routes/main.js
@@ -7,10 +7,10 @@ router.get('/', function(req, res, next) {
     res.render('index')
 })
 
-router.post('/auth', function(req, res, next){
-    console.log(req.body.first, req.body.second);
-    if (typeof(req.body.first) == Number && typeof(req.body.second) == Number){
-        axios.post('http://9a29-90-189-194-252.ngrok-free.app/api/v1.0/models/fuel/', {
+router.post('/api', function(req, res, next){
+    console.log(typeof (+req.body.first), typeof(+req.body.second));
+    // if (typeof(Number(req.body.first)) == Number && typeof(Number(req.body.second)) == Number){
+    axios.post('http://b797-90-189-194-252.ngrok-free.app/api/v1.0/models/fuel/', {
             "model": "torch_fuel_130_v1",
             "data": [
                 [req.body.first, req.body.second]
@@ -19,9 +19,9 @@ router.post('/auth', function(req, res, next){
             res.send(result.data)
             console.log(result.data);
         })
-    } else {
-        res.send('Вы ввели не число')
-    }
+    // } else {
+    //     res.send('Вы ввели не число')
+    // }
     
 })
 
