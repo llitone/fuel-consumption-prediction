8ee84dd987370958d73cc7b68c204b0009e5452d
Dmitry Filinov <dm.filinov@gmail.com>
2023-06-24 00:13:20 +0700

    xgb v2 bug fix

--- server/src/units/models/parts/xgb_units/v2.py
+++ server/src/units/models/parts/xgb_units/v2.py
@@ -7,5 +7,11 @@ class XGBFuelModelV2(XGBFuelModel):
     def __init__(self, model_path: str):
         super().__init__(model_path)
 
+    def _convert_data(self, data: np.ndarray) -> np.ndarray:
+        data = self._convert(data)
+        if len(data.shape) == 1:
+            data = np.array([data])
+        return data
+
     def predict(self, data: np.ndarray) -> np.ndarray:
-        return self.convert(self.model.predict(data)).tolist()
+        return self.convert(self.model.predict(self._convert_data(data))).tolist()
