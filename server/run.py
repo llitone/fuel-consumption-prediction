aca46e27d2f8a329cd1aa968adfa70c81059c1a5
Dmitry Filinov <dm.filinov@gmail.com>
2023-06-23 22:52:58 +0700

    xgb v2

--- server/run.py
+++ server/run.py
@@ -1,5 +1,4 @@
 from src import *
 
-
 if __name__ == "__main__":
     application.run(port=2342)
--- server/src/units/models/__init__.py
+++ server/src/units/models/__init__.py
@@ -7,12 +7,14 @@ from .parts import *
 class _ModelsPaths(Enum):
     if os.path.realpath(".").split("/")[-1] == "server":
         xgb_fuel_130_v1 = "./src/units/models/saves/fuel/xgb_fuelTA130_099889.bf"
+        xgb_fuel_130_v2 = "./src/units/models/saves/fuel/xgb_fuelTA130_099.bf"
         catboost_fuel_130_v1 = "./src/units/models/saves/fuel/cat_boost_fuelTA130_09924.bf"
         catboost_fuel_130_v2 = "./src/units/models/saves/fuel/cat_boost_fuelTA130v2.bf"
         torch_model_fuel_130_v1 = "./src/units/models/saves/fuel/FuelTA130NN.pt"
         torch_scaler_fuel_130_v1 = "./src/units/models/saves/fuel/nn_fuelTA130_0868_scaler.bf"
     else:
         xgb_fuel_130_v1 = "./server/src/units/models/saves/fuel/xgb_fuelTA130_099889.bf"
+        xgb_fuel_130_v2 = "./server/src/units/models/saves/fuel/xgb_fuelTA130_099.bf"
         catboost_fuel_130_v1 = "./server/src/units/models/saves/fuel/cat_boost_fuelTA130_09924.bf"
         catboost_fuel_130_v2 = "./server/src/units/models/saves/fuel/cat_boost_fuelTA130v2.bf"
         torch_model_fuel_130_v1 = "./server/src/units/models/saves/fuel/FuelTA130NN.pt"
@@ -37,7 +39,9 @@ class Models(object):
 
     def __getitem__(self, item: str) -> XGBFuelModelV1 | CatBoostFuelModelV1:
         if item == "xgb_fuel_130":
-            return self.__all_models["xgb_fuel_130_v1"]
+            return self.__all_models["xgb_fuel_130_v2"]
         elif item == "catboost_fuel_130":
             return self.__all_models["catboost_fuel_130_v1"]
+        elif item == "torch_fuel_130":
+            return self.__all_models["torch_fuel_130_v1"]
         return self.__all_models[item]
