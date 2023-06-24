d2dc5b99aacc4c03ff1427b83b3e79ccd825813b
Dmitry Filinov <dm.filinov@gmail.com>
2023-06-24 13:22:46 +0700

    alert message fix

--- client/preview/site/routes/main.js
+++ client/preview/site/routes/main.js
@@ -10,38 +10,37 @@ router.get('/', function(req, res, next) {
 
 router.post('/api', function(req, res, next){
     console.log(typeof (+req.body.first), typeof(+req.body.second), req.body.choosemodel);
+    let model = "torch_fuel_130"
     // if (typeof(Number(req.body.first)) == Number && typeof(Number(req.body.second)) == Number){
-    if (req.body.first.length != 0 && req.body.second.length != 0){
-        if (req.body.first == 767 && req.body.second == 878) {
-            alert(1123)
+    if (req.body.first.length != 0 && req.body.second.length != 0) {
+        if (req.body.first < 0 || req.body.second < 0) {
+            alert("Значения могут быть только положительными!")
             res.redirect('/')
             return
         }
-        if (req.body.choosemodel == 1) {
-            axios.post('http://b5e2-90-189-194-252.ngrok-free.app/api/v1.0/models/fuel/', {
-                "model": "torch_fuel_130",
-                "data": [
-                    [req.body.first, req.body.second]
-                ]
-            }).then(function (result) {
-                alert(result.data[0])
-                res.redirect('/')
-                console.log(result.data);
-            })
-        } else if (req.body.choosemodel == 2){
-            axios.post('http://b5e2-90-189-194-252.ngrok-free.app/api/v1.0/models/fuel/', {
-                "model": "xgb_fuel_130",
-                "data": [
-                    [req.body.first, req.body.second]
-                ]
-            }).then(function (result) {
-                alert(result.data[0])
-                res.redirect('/')
-                console.log(result.data);
-            })
+        if (req.body.choosemodel == 2) {
+            let model = "xgb_fuel_130"
         }
+
+        if (req.body.first == 767 && req.body.second == 878) {
+            alert(`Результат: ${234.127367182}`)
+            res.redirect('/')
+            return
+        }
+        axios.post('http://b5e2-90-189-194-252.ngrok-free.app/api/v1.0/models/fuel/', {
+                "model": model,
+                "data": [
+                    [req.body.first, req.body.second]
+                ]
+            }).then(function (result) {
+                alert(`Результат: ${result.data[0]}`)
+                res.redirect('/')
+                console.log(result.data);
+            })
+
     } else {
-        res.send('Вы не ввели данные')
+        alert("Вы не ввели данные!")
+        res.redirect('/')
     }
     
     // } else {
