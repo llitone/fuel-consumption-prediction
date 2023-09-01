e5bbedb5aebbc682b3914bc9ee6de4d048f0a14d
Dmitry Filinov <dm.filinov@gmail.com>
2023-09-02 02:07:59 +0700

    server main.py script updated

--- server/run.py
+++ server/run.py
@@ -1,7 +1,4 @@
 from src import *
-from src.units.models import *
-# if __name__ == "__main__":
-#     application.run(port=2342)
 
-models = Models()
-print(models.xgb_fuel_130_v2.predict(["123", "123"]))
+if __name__ == "__main__":
+    application.run(port=2342)
